5e82f6129fcd82a504e0cace87cc751d2a688978
AlfredolMacias <frediy079@gmail.com>
2026-04-13 01:30:15 -0600

    Configuración entorno producción 14

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,7 @@
 import * as dotenv from 'dotenv';
 import { defineConfig } from "prisma/config";
 
-// Forzamos la carga del archivo .env (útil para local) 
-// y verificamos el entorno de Railway
-dotenv.config();
-
 const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  // Este log saldrá en tus Runtime Logs de Railway si falla
-  console.error("CRITICAL: DATABASE_URL is missing from process.env");
-}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
